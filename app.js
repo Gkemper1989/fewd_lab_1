af1513cd05fb618155735a02391bd34cad5c3a57
Gkemper1989 <g.kemperweber@gmail.com>
2020-01-28 15:30:22 +0000

    commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var firstName = 'Gustavo'; // declaring a variable firstName
+var favouriteColor = 'Black'; // declaring a variable favouriteColor
+var favouriteFood = 'Steak'; // declaring a variable favouriteFood
+var city = 'Berlin'; // declaring a variable city
+ 
+// function to print the variables defined before in it to the user.
+console.log(firstName);
+console.log(favouriteColor);
+console.log(favouriteFood, city);
+
+//creating a sequel of variables to be showed in a specific order later.
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!'
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+//printing the variables in a specific order to the user.
+console.log(phrase + one + two + three + four + five + six + seven + endingPhrase);
+
